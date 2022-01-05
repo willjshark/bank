1cd36a59be04d5c7bb5205f170cc9bdb9a601da1
willjshark <willjshark@gmial.com>
2022-01-05 10:59:44 +0000

    Added deposit method

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,10 +3,13 @@ require_relative 'statement'
 
 
 def current_balance
- return Statement.all.last.total 
+ return Statement.all.last.total.to_i 
 end
 
-
+def deposit(added_funds)
+balance = added_funds + current_balance
+Statement.create(added_funds, balance)
+end
 
 
 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,13 +1,22 @@
 require 'transaction'
 
-describe current_balance do
+describe :current_balance do
 
   it "returns the user's latest balance" do
     Statement.create(50, 1000)
-    expect(current_balance()).to eq "1000"
+    expect(current_balance()).to eq 1000
     
 end 
 end 
+
+describe :deposit do
+
+    it "it increases the total balance" do
+      Statement.create(50, 1000)
+      deposit(500)
+      expect(current_balance()).to eq 1500    
+  end 
+  end 
 
 
 
