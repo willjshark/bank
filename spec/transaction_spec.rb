07fb3c44babaccc1410ff3769e829d2e62e95d3a
willjshark <willjshark@gmial.com>
2022-01-06 10:51:58 +0000

    Final changes for first draft

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,14 +4,12 @@ require './lib/transaction'
 
 describe :current_balance do
   it "returns the user's latest balance" do
-    Statement.create(0, 1000)
-    expect(current_balance).to eq 1000
+    expect(current_balance).to eq 0
   end
 end
 
 describe :deposit do
   it 'it increases the total balance' do
-    Statement.create(0, 0)
     deposit(500)
     expect(current_balance).to eq 500
   end
@@ -19,7 +17,6 @@ end
 
 describe :withdraw do
   it 'it decreases the total balance' do
-    Statement.create(0, 0)
     deposit(500)
     withdraw(500)
     expect(current_balance).to eq 0
@@ -27,7 +24,6 @@ describe :withdraw do
 
   it "it doesn't decrease the total below 0" do
     expect do
-      Statement.create(0, 0)
       deposit(750)
       withdraw(1000).to output('You do not have enough funds to withdraw that amount.').to_stdout
     end
@@ -37,10 +33,15 @@ end
 describe :statement do
   it 'it produces a list of transactions' do
     expect do
-      Statement.create(0, 0)
       deposit(500)
       withdraw(500)
       statement.to output('|| Date || Amount || Remaining balance ||').to_stdout
+    end
+  end
+
+  it 'it produces a default message if there are no transactions to show' do
+    expect do
+      statement.to output('You have no history of transactions to show').to_stdout
     end
   end
 end
